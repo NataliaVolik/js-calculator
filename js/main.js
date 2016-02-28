41afb49f756c8ccadd139f9934e3db40ae940ae8
Natalya Volik <log.litana@gmail.com>
2016-02-28 18:06:40 +0600

    +button click handler

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+function setScreen(v){
+    $('.screen').html(v);
+}
 
+function buttonClickHandler(eventObject){
+    setScreen(eventObject.currentTarget.className);
+}
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+    $('.button').click(buttonClickHandler);
+});
